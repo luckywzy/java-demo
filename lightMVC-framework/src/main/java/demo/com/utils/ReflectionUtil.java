0765151ec09050ac7eb8d03c5a4f3c060c58bca6
zongyu.wang <zongyu.wang@qunar.com>
2018-10-09 16:46:38 +0800

    传递基本类型的数据已经没有问题

--- lightMVC-framework/src/main/java/demo/com/utils/ReflectionUtil.java
+++ lightMVC-framework/src/main/java/demo/com/utils/ReflectionUtil.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 /**
  * 反射机制 工具类
@@ -41,7 +42,7 @@ public final class ReflectionUtil {
         Object result;
         try {
             method.setAccessible(true);
-            result = method.invoke(obj, args);
+            result = method.invoke(obj,convertParams(args,method));
         } catch (Exception e) {
             LOGGER.error("invoke method failure : {}, {}", method, args, e);
             throw new RuntimeException(e);
@@ -65,4 +66,41 @@ public final class ReflectionUtil {
         }
     }
 
+
+    private static Object[] convertParams(Object[] params, Method method){
+        final int len =  params.length;
+        Object[] resParams = new Object[len];
+        Parameter[] parameters = method.getParameters();
+        for (int i=0; i<len; i++)
+        {
+            resParams[i] = convert(params[i], parameters[i].getType());
+        }
+        return resParams;
+    }
+
+    /**
+     * 对基本类型的参数进行转型操作（略微显得笨拙）
+     * @param o  输入参数
+     * @param clz  转型后的类型
+     * @param <T> 返回值类型
+     * @return
+     */
+    private static <T> T convert(Object o, Class<T> clz) {
+        T cast;
+
+        if (clz.equals(Long.class)) {
+            cast = (T) Long.valueOf(String.valueOf(o));
+        } else if (clz.equals(Boolean.class)) {
+            cast = (T) Boolean.valueOf(String.valueOf(o));
+        } else if (clz.equals(Double.class)) {
+            cast = (T) Double.valueOf(String.valueOf(o));
+        }  else if (clz.equals(Integer.class)) {
+            cast = (T) Integer.valueOf(String.valueOf(o));
+        } else if (clz.equals(Short.class)) {
+            cast = (T) Short.valueOf(String.valueOf(o));
+        }else {
+            cast = clz.cast(o);
+        }
+        return cast;
+    }
 }
--- lightMVC-framework/src/test/java/demo/com/utils/ReflectionUtilTest.java
+++ lightMVC-framework/src/test/java/demo/com/utils/ReflectionUtilTest.java
@@ -2,6 +2,11 @@ package demo.com.utils;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,19 +14,129 @@ import static org.junit.Assert.*;
  */
 public class ReflectionUtilTest {
 
-    static class ServiceA{
+    private static <T> T convert(Object o, Class<T> clz) {
+        T cast;
 
-        public void say(String name){
-            System.out.println("hello :" +name);
+        if (clz.equals(Long.class)) {
+            cast = (T) Long.valueOf(String.valueOf(o));
+        } else if (clz.equals(Boolean.class)) {
+            cast = (T) Boolean.valueOf(String.valueOf(o));
+        } else if (clz.equals(Double.class)) {
+            cast = (T) Double.valueOf(String.valueOf(o));
+        }  else if (clz.equals(Integer.class)) {
+            cast = (T) Integer.valueOf(String.valueOf(o));
+        } else if (clz.equals(Short.class)) {
+            cast = (T) Short.valueOf(String.valueOf(o));
+        }else {
+            cast = clz.cast(o);
+        }
+        return cast;
+    }
+
+    @Test
+    public void testReflect() {
+        ServiceA a = new ServiceA();
+        Class<?> aClass = ClassUtil.loadClass(ServiceA.class.getName());
+        Method[] declaredMethods = aClass.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            System.out.print(method.getName());
+            System.out.print("(");
+            Parameter[] parameters = method.getParameters();
+            for (Parameter parameter : parameters) {
+                System.out.print(parameter.getType().getName() + " " + parameter.getName() + ",");
+            }
+            System.out.println(")");
         }
     }
 
-
-
     @Test
-    public void testReflect(){
-        ServiceA a = new ServiceA();
-        /*ReflectionUtil.invokeMethod(a, )*/
+    public void testToTypes() throws InvocationTargetException, IllegalAccessException {
+        Object args[] = new Object[2];
+        args[0] = "2123214";
+        args[1] = "312312312";
+       helper("toInteger",args );
 
     }
+
+    @Test
+    public void testToBool() throws InvocationTargetException, IllegalAccessException {
+        ServiceA a = new ServiceA();
+        Class<?> aClass = ClassUtil.loadClass(ServiceA.class.getName());
+        Method[] method = aClass.getDeclaredMethods();
+        for (Method mt : method) {
+            if (mt.getName().equals("tobool")) {
+                Parameter[] parameters = mt.getParameters();
+                final int len = parameters.length;
+                Object b = "false";
+                Object params[] = new Object[len];
+                for (int i = 0; i < parameters.length; i++) {
+                    params[i] = convert(b, parameters[i].getType());
+                }
+                Object invoke = mt.invoke(a, params);
+                System.out.println(invoke);
+            }
+        }
+    }
+
+    private void helper(String methodName, Object[] args) throws InvocationTargetException, IllegalAccessException {
+        ServiceA a = new ServiceA();
+        Class<?> aClass = ClassUtil.loadClass(ServiceA.class.getName());
+        Method[] method = aClass.getDeclaredMethods();
+        for (Method mt : method) {
+            if (mt.getName().equals(methodName)) {
+                Parameter[] parameters = mt.getParameters();
+                final int len = parameters.length;
+
+                Object params[] = new Object[len];
+                for (int i = 0; i < parameters.length; i++) {
+                    params[i] = convert(args[i], parameters[i].getType());
+                }
+                Object invoke = mt.invoke(a, params);
+                System.out.println(invoke);
+            }
+        }
+    }
+
+    @Test
+    public void testToLong() throws InvocationTargetException, IllegalAccessException {
+        ServiceA a = new ServiceA();
+        Class<?> aClass = ClassUtil.loadClass(ServiceA.class.getName());
+        Method[] method = aClass.getDeclaredMethods();
+        for (Method mt : method) {
+            if (mt.getName().equals("toLong")) {
+                Parameter[] parameters = mt.getParameters();
+                final int len = parameters.length;
+                Object b = "1234546213456";
+                Object params[] = new Object[len];
+                for (int i = 0; i < parameters.length; i++) {
+                    params[i] = convert(b, parameters[i].getType());
+                }
+                Object invoke = mt.invoke(a, params);
+                System.out.println(invoke);
+            }
+        }
+    }
+
+    static class ServiceA {
+
+        public void say(String name) {
+            System.out.println("hello :" + name);
+        }
+
+        public Integer toInteger(Integer a, Integer b) {
+            System.out.println(a + b);
+            return a + b;
+        }
+
+        public boolean tobool(Boolean b) {
+            System.out.println(b);
+            return b;
+        }
+
+        public Long toLong(Long b) {
+            System.out.println(b);
+            return b;
+        }
+    }
+
 }
