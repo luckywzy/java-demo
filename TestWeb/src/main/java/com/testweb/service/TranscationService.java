8b015a9e0c4679684461eefb75a8a8757b7e276e
nnlover <18792470504@163.com>
2018-11-10 16:10:19 +0800

    添加事务特性

--- TestWeb/src/main/java/com/testweb/service/TranscationService.java
+++ TestWeb/src/main/java/com/testweb/service/TranscationService.java
@@ -1,12 +1,14 @@
 package com.testweb.service;
 
 import demo.com.annotation.MyService;
+import demo.com.annotation.MyTransaction;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @MyService
 public class TranscationService {
 
+    @MyTransaction
     public String tranA(String a){
         log.debug("tranA with args:{}", a);
 
